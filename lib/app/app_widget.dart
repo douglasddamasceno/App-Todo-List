bea996963011f846aa55f1d6af44097301a30a65
Douglas Damasceno <douglasddx@outlook.com>
2025-10-08 22:27:28 -0300

    Configuração de asministração da conexão de db.

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list/app/modules/splash/splash_screen.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
